d4b6cfa22259b03d1d7dc76e1de311ff984a8131
fpolidoro <s231594@studenti.polito.it>
2022-11-04 11:11:26 +0000

    Add script to display an SVG image instead of a d3 chart - Charts are not displayed on mobile devices, therefore it is worth to try displaying a static image and simply attach an event to handle the gesture. This makes it a low-fi question prototype

---
+++ questionnaire/image/script.js
@@ -0,0 +1,38 @@
+var modules = [
+//`https://cdnjs.cloudflare.com/ajax/libs/d3/7.6.1/d3.min.js`,
+//`https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js`,
+//`https://cdnjs.cloudflare.com/ajax/libs/zingtouch/1.0.6/zingtouch.min.js`
+`../../libs/zingtouch.js`,
+`../../libs/d3.v6.js`
+]
+
+Promise.all(
+    modules.map((module, _) =>
+        import(module)
+    )
+).then(() => {
+    console.log(`modules loaded!`)
+    var viewportwidth = window.innerWidth
+    || document.documentElement.clientWidth
+    || document.body.clientWidth;
+
+    var height = (window.innerHeight
+    || document.documentElement.clientHeight
+    || document.body.clientHeight)/2;
+
+    var chart = document.getElementById("chart")
+    var oldK = 0
+    d3.select("#area").call(d3.zoom().on("zoom", function (e){
+        //if(Math.abs(oldK - e.transform.k) > 0.8){
+            console.log(e.transform)
+            if(e.transform.k > 1){
+                console.log(`Zoom in ${e.transform.k}`)
+                chart.src = "cactus-desert-hot.svg"
+            }else{
+                console.log(`Zoom out ${e.transform.k}`)
+                chart.src = "environment-fall-forest.svg"
+            }
+            oldK = e.transform.k
+        //}
+    })) 
+})
