7b9f4605ccf401b1c16cf63a0a8263fe445f7805
fpolidoro <s231594@studenti.polito.it>
2022-12-14 11:36:36 +0000

    Add histogram sprite - Re-organized tree structure of sprite folder

--- questionnaire/sprites/heatmap/hand_colored/script.js
+++ questionnaire/sprites/heatmap/hand_colored/script.js
@@ -1,12 +1,12 @@
 var modules = [
   //`https://cdnjs.cloudflare.com/ajax/libs/d3/7.6.1/d3.min.js`,
   //`https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js`
-  `../../../libs/d3.v6.js`,
-  `../../../libs/jquery-3.6.0-min.js`
+  `../../../../libs/d3.v6.js`,
+  `../../../../libs/jquery-3.6.0-min.js`
 ]
 
 const el = document.getElementById("anim")
-const MIN = 244//250
+const MIN = 246//250
 const MAX = MIN*3//750
 const LAST = MIN+MAX
 
--- questionnaire/sprites/histogram/hand_bw/script.js
+++ questionnaire/sprites/histogram/hand_bw/script.js
@@ -1,13 +1,13 @@
 var modules = [
   //`https://cdnjs.cloudflare.com/ajax/libs/d3/7.6.1/d3.min.js`,
   //`https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js`
-  `../../../libs/d3.v6.js`,
-  `../../../libs/jquery-3.6.0-min.js`
+  `../../../../libs/d3.v6.js`,
+  `../../../../libs/jquery-3.6.0-min.js`
 ]
 
 const el = document.getElementById("anim")
-const MIN = 246//250
-const MAX = MIN*3//750
+const MIN = 256
+const MAX = MIN*3
 const LAST = MIN+MAX
 
 
