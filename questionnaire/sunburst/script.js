529d9734c13df935fd5edbd5844002ed9dcf690a
fpolidoro <s231594@studenti.polito.it>
2022-12-28 15:11:28 +0000

    Add zoom gesture to sunburst - [to do] Make double click go back to initial state and differentiate double click from pinch-zoom

--- questionnaire/sunburst/script.js
+++ questionnaire/sunburst/script.js
@@ -25,73 +25,6 @@ Promise.all(
     var color = //d3.scaleOrdinal(d3.schemePastel1);
     d3.scaleOrdinal(d3.quantize(d3.interpolateRainbow, data.children.length + 1))
 
-    //var partition = d3.partition();
-
-    // var arc = d3.arc()
-    //   .startAngle(d => Math.max(0, Math.min(2 * Math.PI, x(d.x0))))
-    //   .endAngle(d => Math.max(0, Math.min(2 * Math.PI, x(d.x1))))
-    //   .padAngle(d => Math.min((d.x1 - d.x0) / 2, 0.005))
-    //   .padRadius(radius * 1.5)
-    //   .innerRadius(d => d.y0 * radius)
-    //   //.innerRadius(function (d) { return Math.max(0, y(d.y0)); })
-    //   //.outerRadius(function (d) { return Math.max(0, y(d.y1)); });
-    //   .outerRadius(d => Math.max(d.y0 * radius, d.y1 * radius - 1))
-
-
-    // var svg = d3.select("#sun").append("svg")
-    //   .attr("width", width)
-    //   .attr("height", height)
-    //   .append("g")
-    //   .attr("transform", "translate(" + width / 2 + "," + (height / 2) + ")");
-
-    // root = d3.hierarchy(data);
-    // root.sum(function (d) { return !d.children || d.children.length === 0 ? d.duration : 0; });
-    // svg.selectAll("path")
-    //   .data(partition(root).descendants())
-    //   .enter().append("path")
-    //   .attr("d", arc)
-    //   .style("fill", function (d) {
-    //     console.log(d)
-    //     console.log(`d: ${d.data.name}, children: ${d.children ? 'YES' : 'NO'}`)
-    //     return color((d.children ? d : d.parent).data.name);
-    //   })
-    //   .on("click", click)
-    //   .append("title")
-    //   .text((d) => `${(d.children ? d : d.parent).data.name}`); /*d.data.duration*/
-
-    // const label = svg.append("g")
-    //   .attr("pointer-events", "none")
-    //   .attr("text-anchor", "middle")
-    //   .style("user-select", "none")
-    //   .selectAll("text")
-    //   .data(root.descendants())
-    //   .join("text")
-    //   .attr("dy", "0.35em")
-    //   //.attr("fill-opacity", d => +labelVisible(d.current))
-    //   .attr("transform", d => {
-    //     const x = (d.x0 + d.x1) / 2 * 180 / Math.PI;
-    //     const y = (d.y0 + d.y1) / 2 * radius;
-    //     return `rotate(${x - 90}) translate(${y},0) rotate(${x < 180 ? 0 : 180})`;
-    //   })
-    //   .text(d => d.data.name);
-
-    // function click(d) {
-    //   svg.transition()
-    //     .duration(750)
-    //     .tween("scale", function () {
-    //       var xd = d3.interpolate(x.domain(), [d.x0, d.x1]),
-    //         yd = d3.interpolate(y.domain(), [d.y0, 1]),
-    //         yr = d3.interpolate(y.range(), [d.y0 ? 20 : 0, radius]);
-    //       return function (t) { x.domain(xd(t)); y.domain(yd(t)).range(yr(t)); };
-    //     })
-    //     .selectAll("path")
-    //     .attrTween("d", function (d) { return function () { return arc(d); }; });
-    // }
-
-    // d3.select(self.frameElement).style("height", height + "px");
-
-    //------------------------------
-    const format = d3.format(",d")
     const arc = d3.arc()
       .startAngle(d => d.x0)
       .endAngle(d => d.x1)
@@ -131,41 +64,45 @@ Promise.all(
         .attr("fill-opacity", d => arcVisible(d.current) ? (d.children ? 0.6 : 0.4) : 0)
         .attr("pointer-events", d => arcVisible(d.current) ? "auto" : "none")
         .attr("d", d => arc(d.current));
-
-      /*path.append("title")
-      .text(d => `${d.ancestors().map(d => d.data.name).reverse().join("/")}\n${format(d.data.duration)}`);*/
   
     path.filter(d => d.children)
         .style("cursor", "pointer")
         .on("click", clicked);
   
     const label = g.append("g")
-        .attr("pointer-events", "none")
-        .attr("text-anchor", "middle")
-        .style("user-select", "none")
+      .attr("pointer-events", "none")
+      .attr("text-anchor", "middle")
+      .style("user-select", "none")
       .selectAll("text")
       .data(root.descendants())
       .join("text")
-        .attr("dy", "0.35em")
-        .attr("fill-opacity", d => +labelVisible(d.current))
-        .attr("transform", d => labelTransform(d.current))
-        .text(d => {
-          console.log(d)
-          return `${d.children ? d.data.name : d.data.duration}`
-        });
+      .attr("dy", "0.35em")
+      .attr("fill-opacity", d => +labelVisible(d.current))
+      .attr("transform", d => labelTransform(d.current))
+      .text(d => {
+        console.log(d)
+        return `${d.children ? d.data.name : d.data.duration}`
+      });
   
     const parent = g.append("circle")
-        .datum(root)
-        .attr("r", radius*0.97)
-        .attr("fill", "none")
-        .attr("pointer-events", "all")
-        .on("click", clicked);
+      .datum(root)
+      .attr("r", radius*0.97)
+      .attr("fill", "none")
+      .attr("pointer-events", "all")
+      .on("click", clicked)
+      .call(d3.zoom().on("zoom", e => {
+        if(e.transform.k < 1){
+          console.log('zoomed')
+          return clicked
+        }
+      }))
+
     parent.append("g")
-    .attr("dy", "0.35em")
-    .text(d => {
-      console.log(d)
-      return `${d.data.name}`
-    })
+      .attr("dy", "0.35em")
+      .text(d => {
+        console.log(d)
+        return `${d.data.name}`
+      })
   
     function clicked(event, p) {
       parent.datum(p.parent || root);
