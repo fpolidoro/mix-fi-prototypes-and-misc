37e35b06a6c60d10b89a38adc211aa532776bd1d
fpolidoro <s231594@studenti.polito.it>
2023-06-08 22:22:59 +0100

    Handle pinch-in on colored sunburst

--- questionnaire/sprites/sunburst/activity_color/script.js
+++ questionnaire/sprites/sunburst/activity_color/script.js
@@ -7,6 +7,7 @@ var modules = [
 
 const el = document.getElementById("anim")
 const ia1 = document.getElementById("ia-1")
+const ia3 = document.getElementById("ia-3")
 const MIN_1 = 272
 const MAX_1 = MIN_1*(16)
 const LAST_1 = MIN_1+MAX_1
@@ -15,6 +16,7 @@ const MIN_2 = 272
 const MAX_2 = MIN_2*3
 let lastPos = 0
 
+const MID = 272*4
 
 Promise.all(
   modules.map((module, _) =>
@@ -24,28 +26,39 @@ Promise.all(
   let lastPosY = 0
   let startK = 0
 
+  let touchCount = 2//0
+
+  /*document.querySelector('#ia-3').addEventListener('touchstart', (e) => {
+    touchCount = e.touches.length;
+  })*/
+
   function zoom(id, area){
     const zoom = d3.zoom()
     .on("start", e => {
-      //console.info("zoom start")
-      el.style.backgroundPositionX = `0px`
-      el.style.backgroundPositionY = `${lastPosY}px`
-      startK = e.transform.k
+      if(Math.abs(lastPos) === MID && touchCount > 1){
+        console.info("zoom start")
+        el.style.backgroundPositionX = `0px`
+        el.style.backgroundPositionY = `${lastPosY}px`
+        startK = e.transform.k
+      }
     })
     .on("zoom", e => {
-      if(e.transform.k - startK > 0 && lastPosY <= -MIN_1){
-        //console.log(`Zoom in ${e.transform.k}`)
-        el.style.backgroundPositionY = `${lastPosY += MIN_1}px`
-      }else if(e.transform.k - startK < 0 && lastPosY >= -MAX_1){
-        //console.log(`Zoom out ${e.transform.k}`)
-        el.style.backgroundPositionY = `${lastPosY -= MIN_1}px`
-      }/*else{
-        console.warn(`Zoom ${e.transform.k < 1 ? 'out':'in'}: ${e.transform.k}\nlastPosY: ${lastPosY}`)
-      }*/
-      //console.log(`zoom, bpy: ${el.style.backgroundPositionY}`)
+      if(Math.abs(lastPos) === MID && touchCount > 1){
+        if(e.transform.k - startK > 0 && lastPosY <= -MIN_1){
+          //console.log(`Zoom in ${e.transform.k}`)
+          el.style.backgroundPositionY = `${lastPosY += MIN_1}px`
+        }else if(e.transform.k - startK < 0 && lastPosY >= -MAX_1){
+          console.log(`Zoom out ${e.transform.k}`)
+          el.style.backgroundPositionY = `${lastPosY -= MIN_1}px`
+        }/*else{
+          console.warn(`Zoom ${e.transform.k < 1 ? 'out':'in'}: ${e.transform.k}\nlastPosY: ${lastPosY}`)
+        }*/
+        //console.log(`zoom, bpy: ${el.style.backgroundPositionY}`)
+      }
     })
     .on("end", e => {
-      //console.log(`zoom`)
+      if(Math.abs(lastPos) === MID && touchCount > 1){
+        //console.log(`zoom`)
       if(e.transform.k - startK > 0 && lastPosY <= -MIN_1){
         console.log(`Zoom in ${e.transform.k}`)
         el.style.backgroundPositionY = `${lastPosY = -LAST_1}px`
@@ -55,11 +68,10 @@ Promise.all(
       }
       area.style.pointerEvents = 'none'
       //console.info(`zoom end, bpy: ${el.style.backgroundPositionY}`)
+      }
     })
     d3.select(`#${id}`).call(zoom)
   }
-
-  //zoom('ia-1', ia1)
   
   /*d3.select("#ia-2").on("click", () => {
     el.style.backgroundPositionY = '0px'
@@ -77,25 +89,31 @@ Promise.all(
   })*/
 
   d3.select("#ia-2").on("click", () => {
-    el.style.backgroundPositionY = '0px'
-    console.log(`iterations: ${Math.trunc(MAX_2/MIN_2)}`)
-    let lastPosY = 0
-    function animate(i) {
-      setTimeout(() => {
-        lastPosY -= MIN_2
-        console.log(`i: ${i}, pos: ${lastPosY}`)
-        el.style.backgroundPositionY = `${lastPosY}px`
-        lastPos = lastPosY
-        if (i++ < Math.trunc(MAX_2/MIN_2)){
-          animate(i)
-        }   //decrement i and call animate again if i > 0
-      }, (MAX_2/MIN_2)*50)
-    }
-    animate(0)
+    if(lastPos%MAX_1 === 0){  //animate only if we are on the first frame
+      //document.getElementById("ia-2").style.pointerEvents = 'auto'
+      el.style.backgroundPositionY = '0px'
+      console.log(`iterations: ${Math.trunc(MAX_2/MIN_2)}`)
+      let lastPosY = 0
+      function animate(i) {
+        setTimeout(() => {
+          lastPosY -= MIN_2
+          console.log(`i: ${i}, pos: ${lastPosY}`)
+          el.style.backgroundPositionY = `${lastPosY}px`
+          lastPos = lastPosY
+          if (i++ < Math.trunc(MAX_2/MIN_2)){
+            animate(i)
+          }   //decrement i and call animate again if i > 0
+        }, (MAX_2/MIN_2)*50)
+      }
+      animate(0)
+    }/*else{
+      document.getElementById("ia-2").style.pointerEvents = 'none'
+    }*/
   })
 
   d3.select("#ia-1").on("click", () => {
     if(lastPos%MAX_1 !== 0){  //disable this interactive area when we are at the very beginning or at the very end of the spritesheet (i.e. we are on frame 0)
+      //document.getElementById("ia-1").style.pointerEvents = 'auto'
       console.log(`iterations: ${Math.trunc(MAX_2/MIN_2)}`)
       let lastPosY = lastPos
       let endPosY = lastPosY - MIN_2*3
@@ -112,11 +130,15 @@ Promise.all(
           }else{
             console.warn(`i=${i} < ${(endPosY-lastPosY)/MIN_2}, stopping animation`)
           }
-        }, iterations*30)
+        }, iterations*50)
       }
       animate(0)
-      }
+    }/*else{
+      document.getElementById("ia-1").style.pointerEvents = 'none'
+    }*/
   })
+
+  zoom('ia-3', ia3)
 
   d3.select("#reset").on("click", () => {
     el.style.backgroundPositionX = '0px'
