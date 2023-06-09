6c96449aecb094372cffb466d61a27e26b74da3d
fpolidoro <s231594@studenti.polito.it>
2023-06-09 19:13:54 +0100

    Add rxjs to colored sunburst - [to do] Handle zoom with rxjs - Handle clicks on ia-1 and ia-2 with rxjs

--- questionnaire/sprites/sunburst/activity_color/script.js
+++ questionnaire/sprites/sunburst/activity_color/script.js
@@ -2,11 +2,13 @@ var modules = [
   //`https://cdnjs.cloudflare.com/ajax/libs/d3/7.6.1/d3.min.js`,
   //`https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js`
   `../../../../libs/d3.v6.js`,
-  `../../../../libs/jquery-3.6.0-min.js`
+  `../../../../libs/jquery-3.6.0-min.js`,
+  //`https://npmcdn.com/@reactivex/rxjs@5.0.0-beta.6/dist/global/Rx.umd.js`
 ]
 
 const el = document.getElementById("anim")
 const ia1 = document.getElementById("ia-1")
+const ia2 = document.getElementById("ia-2")
 const ia3 = document.getElementById("ia-3")
 const MIN_1 = 272
 const MAX_1 = MIN_1*(16)
@@ -72,23 +74,8 @@ Promise.all(
     })
     d3.select(`#${id}`).call(zoom)
   }
-  
-  /*d3.select("#ia-2").on("click", () => {
-    el.style.backgroundPositionY = '0px'
-    console.log(`iterations: ${Math.trunc(MAX_2/MIN_2)}`)
-    let lastPosY = MAX_2
-    function animate(i) {
-      setTimeout(() => {
-        lastPosY -= MIN_2
-        console.log(`i: ${i}, pos: ${lastPosY}`)
-        el.style.backgroundPositionY = `${lastPosY}px`
-        if (--i > 1) animate(i)   //decrement i and call animate again if i > 0
-      }, (MAX_2/MIN_2)*30)
-    }
-    animate(Math.trunc(MAX_2/MIN_2))
-  })*/
 
-  d3.select("#ia-2").on("click", () => {
+  /*d3.select("#ia-2").on("click", () => {
     if(lastPos%MAX_1 === 0){  //animate only if we are on the first frame
       //document.getElementById("ia-2").style.pointerEvents = 'auto'
       el.style.backgroundPositionY = '0px'
@@ -100,45 +87,44 @@ Promise.all(
           console.log(`i: ${i}, pos: ${lastPosY}`)
           el.style.backgroundPositionY = `${lastPosY}px`
           lastPos = lastPosY
+          lastPosY$.next(lastPos)
           if (i++ < Math.trunc(MAX_2/MIN_2)){
             animate(i)
           }   //decrement i and call animate again if i > 0
         }, (MAX_2/MIN_2)*50)
       }
       animate(0)
-    }/*else{
-      document.getElementById("ia-2").style.pointerEvents = 'none'
-    }*/
-  })
+    }//else{
+      //document.getElementById("ia-2").style.pointerEvents = 'none'
+    //}
+  })*/
 
-  d3.select("#ia-1").on("click", () => {
-    if(lastPos%MAX_1 !== 0){  //disable this interactive area when we are at the very beginning or at the very end of the spritesheet (i.e. we are on frame 0)
+  //zoom('ia-3', ia3)
+
+  d3.select('#ia-3').on("click", () => {
+    //if(lastPos%MAX_1 !== 0){  //disable this interactive area when we are at the very beginning or at the very end of the spritesheet (i.e. we are on frame 0)
       //document.getElementById("ia-1").style.pointerEvents = 'auto'
-      console.log(`iterations: ${Math.trunc(MAX_2/MIN_2)}`)
       let lastPosY = lastPos
-      let endPosY = lastPosY - MIN_2*3
+      let endPosY = 0
       let iterations = Math.abs(Math.trunc((endPosY-lastPosY)/MIN_2))
+      console.log(`iterations: ${Math.abs(Math.trunc((endPosY-lastPosY)/MIN_2))}`)
       console.log(`lastY: ${lastPosY}, endY: ${endPosY}`)
       function animate(i) {
         setTimeout(() => {
-          lastPosY -= MIN_2
+          lastPosY += MIN_2
           console.log(`i: ${i}, pos: ${lastPosY}`)
           el.style.backgroundPositionY = `${lastPosY}px`
           lastPos = lastPosY
-          if (i++ < iterations){
+          if (i-- > 0){
             animate(i)   //decrement i and call animate again if i > 0
           }else{
-            console.warn(`i=${i} < ${(endPosY-lastPosY)/MIN_2}, stopping animation`)
+            console.warn(`i=${i} < ${iterations}, stopping animation`)
           }
         }, iterations*50)
       }
-      animate(0)
-    }/*else{
-      document.getElementById("ia-1").style.pointerEvents = 'none'
-    }*/
+      animate(iterations-1)
+    //}
   })
-
-  zoom('ia-3', ia3)
 
   d3.select("#reset").on("click", () => {
     el.style.backgroundPositionX = '0px'
@@ -147,3 +133,72 @@ Promise.all(
     setTimeout(() => { el.style.animation = "" }, 1500)
   })
 })
+
+let script = document.createElement('script')
+script.setAttribute('src', 'https://unpkg.com/rxjs@7.5.5/dist/bundles/rxjs.umd.js')
+script.setAttribute('id', "rxscript")
+document.body.appendChild(script) 
+
+// now wait for it to load...
+script.onload = () => {
+  // script has loaded, you can now use it safely
+  console.warn('thank me later')
+  // ... do something with the newly loaded script
+  
+  /*const source = window.rxjs.interval(1000);
+  //sample last emitted value from source every 2s
+  const example = window.rxjs.interval(2000);
+  //output: 2..4..6..8..
+  const subscribe = example.subscribe((val) => console.log(val));*/
+  const lastPosY$ = new window.rxjs.BehaviorSubject(0)
+  lastPosY$.subscribe(v => console.log(`lastPosY$ emitted ${v}`))
+
+  const ia1Click$ = window.rxjs.fromEvent(ia1, 'click');
+  ia1Click$.pipe(
+    window.rxjs.tap(() => console.log(`ia-1 clicked`)),
+    window.rxjs.exhaustMap(() => window.rxjs.interval(100).pipe(
+      window.rxjs.withLatestFrom(lastPosY$.pipe(
+        window.rxjs.map((lpY => {
+          console.log(`${lpY}`)
+          return lpY%MAX_1
+        }))
+      )),
+      window.rxjs.takeWhile(([i, lpY]) => {
+        console.info(`ia-1 ${lpY !== 0 ? 'enabled' : 'disabled'} because lpY=${lpY}`)
+        return lpY !== 0
+      }),//disable this interactive area when we are at the very beginning or at the very end of the spritesheet (i.e. we are on frame 0)
+      window.rxjs.take(5)
+    ))
+  ).subscribe(([i, lPosY]) => {
+    console.log(`animating IA-1: ${i}, ${lPosY+MIN_1*i}`)
+    el.style.backgroundPositionY = `-${lPosY+MIN_1*i}px`
+    if(i === 4){
+      console.info(`updating lastPosY$ with ${(lPosY+MIN_1*i)%MAX_1}`)
+      lastPosY$.next((lPosY+MIN_1*i)%MAX_1)
+    }
+  })
+
+  const ia2Click$ = window.rxjs.fromEvent(ia2, 'click')
+  ia2Click$.pipe(
+    window.rxjs.exhaustMap(() => window.rxjs.interval(100).pipe(
+      window.rxjs.withLatestFrom(lastPosY$.pipe(
+        window.rxjs.map((lpY => {
+          console.log(`${lpY}`)
+          return lpY%MAX_1
+        }))
+      )),
+      window.rxjs.takeWhile(([i, lpY]) => {
+        console.info(`ia-1 ${lpY === 0 ? 'enabled' : 'disabled'} because lpY=${lpY}`)
+        return lpY === 0
+      }),//disable this interactive area when we are at the very beginning or at the very end of the spritesheet (i.e. we are on frame 0)
+      window.rxjs.take(5)
+    ))
+  ).subscribe(([i, lPosY]) => {
+    console.log(`animating IA-1: ${i}, ${lPosY+MIN_1*i}`)
+    el.style.backgroundPositionY = `-${lPosY+MIN_1*i}px`
+    if(i === 4){
+      console.info(`updating lastPosY$ with ${(lPosY+MIN_1*i)%MAX_1}`)
+      lastPosY$.next((lPosY+MIN_1*i)%MAX_1)
+    }
+  })
+}      
