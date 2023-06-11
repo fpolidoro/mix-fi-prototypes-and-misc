ca0e626e8c937cf60eef30b8014a5a28c0f48212
fpolidoro <s231594@studenti.polito.it>
2023-06-11 22:47:11 +0100

    Add colored sunburst w/ pinch - Pinch is detected with two fingers but it doesn't delete the events properly

---
+++ questionnaire/sprites/sunburst/activity_color_pinch/script.js
@@ -0,0 +1,426 @@
+var modules = [
+  //`https://cdnjs.cloudflare.com/ajax/libs/d3/7.6.1/d3.min.js`,
+  //`https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js`
+  `../../../../libs/d3.v6.js`,
+  `../../../../libs/jquery-3.6.0-min.js`,
+  //`https://npmcdn.com/@reactivex/rxjs@5.0.0-beta.6/dist/global/Rx.umd.js`
+]
+
+const el = document.getElementById("anim")
+const ia1 = document.getElementById("ia-1")
+const ia2 = document.getElementById("ia-2")
+const ia3 = document.getElementById("ia-3")
+const MIN_1 = 272
+const MAX_1 = MIN_1*(16)
+const LAST_1 = MIN_1+MAX_1
+
+const MIN_2 = 272
+const MAX_2 = MIN_2*3
+let lastPos = 0
+
+const MID = 272*4
+
+Promise.all(
+  modules.map((module, _) =>
+    import(module)
+  )
+).then(() => {
+  let lastPosY = 0
+  let startK = 0
+
+  let touchCount = 2//0
+
+  /*document.querySelector('#ia-3').addEventListener('touchstart', (e) => {
+    touchCount = e.touches.length;
+  })*/
+
+  function zoom(id, area){
+    const zoom = d3.zoom()
+    .on("start", e => {
+      if(Math.abs(lastPos) === MID && touchCount > 1){
+        console.info("zoom start")
+        el.style.backgroundPositionX = `0px`
+        el.style.backgroundPositionY = `${lastPosY}px`
+        startK = e.transform.k
+      }
+    })
+    .on("zoom", e => {
+      if(Math.abs(lastPos) === MID && touchCount > 1){
+        /*if(e.transform.k - startK > 0 && lastPosY <= -MIN_1){
+          //console.log(`Zoom in ${e.transform.k}`)
+          el.style.backgroundPositionY = `${lastPosY += MIN_1}px`
+        }else */if(e.transform.k - startK < 0 && lastPosY >= -MAX_1){
+          console.log(`Zoom out ${e.transform.k}`)
+          el.style.backgroundPositionY = `${lastPosY -= MIN_1}px`
+        }/*else{
+          console.warn(`Zoom ${e.transform.k < 1 ? 'out':'in'}: ${e.transform.k}\nlastPosY: ${lastPosY}`)
+        }*/
+        //console.log(`zoom, bpy: ${el.style.backgroundPositionY}`)
+      }
+    })
+    .on("end", e => {
+      if(Math.abs(lastPos) === MID && touchCount > 1){
+        //console.log(`zoom`)
+      /*if(e.transform.k - startK > 0 && lastPosY <= -MIN_1){
+        console.log(`Zoom in ${e.transform.k}`)
+        el.style.backgroundPositionY = `${lastPosY = -LAST_1}px`
+      }else*/ if(e.transform.k - startK < 0 && lastPosY >= -MAX_1){
+        console.log(`Zoom out ${e.transform.k}`)
+        el.style.backgroundPositionY = `${lastPosY = 0}px`
+      }
+      //area.style.pointerEvents = 'none'
+      //console.info(`zoom end, bpy: ${el.style.backgroundPositionY}`)
+      }
+    })
+    d3.select(`#${id}`).call(zoom)
+  }
+
+  //zoom('ia-3', ia3)
+
+  /*d3.select('#ia-3').on("click", () => {
+    //if(lastPos%MAX_1 !== 0){  //disable this interactive area when we are at the very beginning or at the very end of the spritesheet (i.e. we are on frame 0)
+      //document.getElementById("ia-1").style.pointerEvents = 'auto'
+      let lastPosY = lastPos
+      let endPosY = 0
+      let iterations = Math.abs(Math.trunc((endPosY-lastPosY)/MIN_2))
+      console.log(`iterations: ${Math.abs(Math.trunc((endPosY-lastPosY)/MIN_2))}`)
+      console.log(`lastY: ${lastPosY}, endY: ${endPosY}`)
+      function animate(i) {
+        setTimeout(() => {
+          lastPosY += MIN_2
+          console.log(`i: ${i}, pos: ${lastPosY}`)
+          el.style.backgroundPositionY = `${lastPosY}px`
+          lastPos = lastPosY
+          if (i-- > 0){
+            animate(i)   //decrement i and call animate again if i > 0
+          }else{
+            console.warn(`i=${i} < ${iterations}, stopping animation`)
+          }
+        }, iterations*50)
+      }
+      animate(iterations-1)
+    //}
+  })*/
+
+  d3.select("#reset").on("click", () => {
+    el.style.backgroundPositionX = '0px'
+    el.style.backgroundPositionY = `0px`
+    ia1.style.pointerEvents = 'unset'
+    setTimeout(() => { el.style.animation = "" }, 1500)
+  })
+
+
+  let script = document.createElement('script')
+  script.setAttribute('src', 'https://unpkg.com/rxjs@7.5.5/dist/bundles/rxjs.umd.js')
+  script.setAttribute('id', "rxscript")
+  document.body.appendChild(script) 
+
+  // now wait for it to load...
+  script.onload = () => {
+    // script has loaded, you can now use it safely
+    console.warn('thank me later')
+    // ... do something with the newly loaded script
+    
+    /*const source = window.rxjs.interval(1000);
+    //sample last emitted value from source every 2s
+    const example = window.rxjs.interval(2000);
+    //output: 2..4..6..8..
+    const subscribe = example.subscribe((val) => console.log(val));*/
+    const lastPosY$ = new window.rxjs.BehaviorSubject(0)
+    lastPosY$.subscribe(v => console.log(`lastPosY$ emitted ${v}`))
+
+    /*const ia1Click$ = window.rxjs.fromEvent(ia1, 'click');
+    ia1Click$.pipe(
+      window.rxjs.exhaustMap(() => window.rxjs.interval(100).pipe(
+        window.rxjs.withLatestFrom(lastPosY$.pipe(
+          window.rxjs.map((lpY => {
+            //console.log(`${lpY}`)
+            return lpY%MAX_1
+          }))
+        )),
+        window.rxjs.takeWhile(([i, lpY]) => {
+          //console.info(`ia-1 ${lpY !== 0 ? 'enabled' : 'disabled'} because lpY=${lpY}`)
+          return lpY !== 0
+        }),//disable this interactive area when we are at the very beginning or at the very end of the spritesheet (i.e. we are on frame 0)
+        window.rxjs.take(5)
+      ))
+    ).subscribe(([i, lPosY]) => {
+      //console.log(`animating IA-1: ${i}, ${lPosY+MIN_1*i}`)
+      el.style.backgroundPositionY = `-${lPosY+MIN_1*i}px`
+      if(i === 4){
+        console.info(`updating lastPosY$ with ${(lPosY+MIN_1*i)%MAX_1}`)
+        lastPosY$.next((lPosY+MIN_1*i)%MAX_1)
+      }
+    })*/
+
+    const ia2Click$ = window.rxjs.fromEvent(ia2, 'click')
+    ia2Click$.pipe(
+      window.rxjs.exhaustMap(() => window.rxjs.interval(100).pipe(
+        window.rxjs.withLatestFrom(lastPosY$.pipe(
+          window.rxjs.map((lpY => {
+            console.log(`${lpY}`)
+            return lpY%MAX_1
+          }))
+        )),
+        window.rxjs.takeWhile(([i, lpY]) => {
+          console.info(`ia-2 ${lpY === 0 ? 'enabled' : 'disabled'} because lpY=${lpY}`)
+          return lpY === 0
+        }),//disable this interactive area when we are at the very beginning or at the very end of the spritesheet (i.e. we are on frame 0)
+        window.rxjs.take(5)
+      ))
+    ).subscribe(([i, lPosY]) => {
+      console.log(`animating IA-2: ${i}, ${lPosY+MIN_1*i}`)
+      el.style.backgroundPositionY = `-${lPosY+MIN_1*i}px`
+      if(i === 4){
+        console.info(`updating lastPosY$ with ${(lPosY+MIN_1*i)%MAX_1}`)
+        lastPosY$.next((lPosY+MIN_1*i)%MAX_1)
+      }
+    })
+
+    /*const ia1Zoom$ = window.rxjs.fromEvent(ia1, 'dblclick')
+    ia1Zoom$.pipe(
+      window.rxjs.exhaustMap(() => window.rxjs.interval(100).pipe(
+        window.rxjs.withLatestFrom(lastPosY$.pipe(
+          window.rxjs.map((lpY => {
+            console.log(`${lpY}`)
+            return lpY%MAX_1
+          }))
+        )),
+        window.rxjs.takeWhile(([i, lpY]) => {
+          console.info(`ia-3 ${Math.abs(lpY) === MID ? 'enabled' : 'disabled'} because lpY=${lpY}`)
+          return Math.abs(lpY) === MID
+        }),//disable this interactive area when we are at the very beginning or at the very end of the spritesheet (i.e. we are on frame 0)
+        window.rxjs.take(5)
+      ))
+    ).subscribe(([i, lPosY]) => {
+      console.log(`animating IA-3: ${i}, ${lPosY-MIN_1*i}`)
+      el.style.backgroundPositionY = `-${lPosY-MIN_1*i}px`
+      if(i === 4){
+        console.info(`updating lastPosY$ with ${(lPosY-MIN_1*i)%MAX_1}`)
+        lastPosY$.next((lPosY-MIN_1*i)%MAX_1)
+      }
+    })*/
+  
+    // How fast does the user has to click
+    // so that it counts as double click
+    const doubleClickDuration = 200;
+
+    // Create a stream out of the mouse click event.
+    const leftClick$ = window.rxjs.fromEvent(ia1, 'click')
+    // We are only interested in left clicks, so we filter the result down
+    .pipe(window.rxjs.filter((event) => event.button === 0));
+
+  // We have two things to consider in order to detect single or
+  // or double clicks.
+
+    // 1. We debounce the event. The event will only be forwared 
+    // once enough time has passed to be sure we only have a single click
+    const debounce$ = leftClick$.pipe(window.rxjs.debounceTime(doubleClickDuration));
+
+    // 2. We also want to abort once two clicks have come in.
+    const clickLimit$ = leftClick$.pipe(
+      window.rxjs.bufferCount(2),
+    );
+
+
+    // Now we combine those two. The gate will emit once we have 
+    // either waited enough to be sure its a single click or
+    // two clicks have passed throug
+    const bufferGate$ = window.rxjs.race(debounce$, clickLimit$).pipe(
+      // We are only interested in the first event. After that
+      // we want to restart.
+      window.rxjs.first(),
+      window.rxjs.repeat(),
+    );
+
+    // Now we can buffer the original click stream until our
+    // buffer gate triggers.
+    leftClick$.pipe(
+      window.rxjs.buffer(bufferGate$),
+      // Here we map the buffered events into the length of the buffer
+      // If the user clicked once, the buffer is 1. If he clicked twice it is 2
+      window.rxjs.map(clicks => clicks.length),
+      window.rxjs.tap((clicks) => console.log(`clicks: ${clicks}`)),
+      window.rxjs.exhaustMap((clicks) => window.rxjs.interval(100).pipe(
+        window.rxjs.map((i) => [clicks, i]),
+        window.rxjs.withLatestFrom(lastPosY$.pipe(
+          window.rxjs.map((lpY => {
+            //console.log(`${lpY}`)
+            return lpY%MAX_1
+          }))
+        )),
+        window.rxjs.takeWhile(([[clicks, i], lpY]) => {
+          //console.info(`ia-1 ${lpY !== 0 ? 'enabled' : 'disabled'} because lpY=${lpY}`)
+          return lpY !== 0
+        }),//disable this interactive area when we are at the very beginning or at the very end of the spritesheet (i.e. we are on frame 0)
+        window.rxjs.take(5)
+      ))
+    ).subscribe(([[clicks, i], lPosY]) => {
+      let pos
+
+      if(clicks > 1){
+        if(Math.abs(lPosY) === MID){
+          pos = lPosY-MIN_1*i
+          console.warn(`Double click`)
+        }
+      }else{
+        pos = lPosY+MIN_1*i
+        console.log(`Single click`)
+      }
+
+      if(clicks === 1 || (clicks > 1 && Math.abs(lPosY) === MID)){
+        el.style.backgroundPositionY = `-${pos}px`
+        if(i === 4){
+          console.info(`updating lastPosY$ with ${pos}`)
+          lastPosY$.next(pos)
+        }
+      }
+    });
+
+
+    var eventMap = new Map()
+    var mousemove = window.rxjs.merge(
+      window.rxjs.fromEvent(document, 'mousemove').pipe(
+        window.rxjs.tap(() => console.log(`mousemove`)),
+        window.rxjs.map((e) => { e.preventDefault(); return e; })),
+      window.rxjs.fromEvent(document, 'touchmove')).pipe(
+        window.rxjs.tap((e) => {
+          console.log(`touchmove, touches: ${e.touches?.length ?? 0}`)
+        }),
+        window.rxjs.map((e) => e.touches !== undefined ? e.touches : {0: e, length: 1})
+    )
+    var mouseup = window.rxjs.merge(
+      window.rxjs.fromEvent(document, 'mouseup'),
+      window.rxjs.fromEvent(document, 'touchend')
+    )
+    var mousedown = window.rxjs.merge(
+      window.rxjs.fromEvent(document, 'mousedown').pipe(
+        window.rxjs.tap(() => console.log(`mousedown`))
+      ),
+      window.rxjs.fromEvent(document, 'touchstart').pipe(
+        window.rxjs.tap(() => console.log(`touchstart`)),
+        window.rxjs.map((e) => {
+          //console.log(e)
+          
+          var rect = e.target.getBoundingClientRect();
+          /*e.offsetX = e.touches[0].pageX - rect.left;
+          e.offsetY = e.touches[0].pageY - rect.top;*/
+          return {
+            touches: e.touches,
+            rect: rect
+          }
+        })
+      )
+    ).pipe(
+      window.rxjs.mergeMap((start) => {
+        return mousemove.pipe(
+          window.rxjs.tap(mm => console.log(mm)),
+          window.rxjs.map(mm => {
+            for(let k in Object.keys(mm)){
+              console.log(start.touches)
+              if(!isNaN(k)){
+                //console.log(mm[k])
+                eventMap.set(mm[k].identifier, {
+                  left: mm[k].clientX - (/*start.touches[k].pageX - */start.rect.left),
+                  top: mm[k].clientY - (/*start.touches[k].pageY - */start.rect.top),
+                  endX: mm[k].clientX,
+                  endY: mm[k].clientY,
+                })
+              }
+            }
+
+            return {
+              events: eventMap,
+              touches: mm.length
+            }
+          }),
+          window.rxjs.takeUntil(mouseup.pipe(
+            window.rxjs.tap((mmend) => {
+              console.log(`touchup`)
+              console.log(mmend)
+              //eventMap.delete(mmend.touches)
+              if(mmend.touches !== undefined){
+                console.log(Object.keys(mmend.touches))
+                for(let k in Object.keys(mmend.touches)){
+                  if(!isNaN(k)){
+                    console.log(mmend.touches[k])
+                    eventMap.delete(mmend.touches[k].identifier)
+                    console.log(`removing ${mmend.touches[k].identifier}`)
+                  }
+                }
+                if(mmend.touches.length === 0 && eventMap.size > 0){
+                  eventMap.clear()
+                }
+              }else{
+                eventMap.clear()
+              }
+
+              console.log(eventMap)
+            })
+          ))
+        )
+      })
+    ).subscribe(pos => {
+      console.log(pos)
+      //console.log(`pos top: ${pos.top}`)
+      //console.log(`pos left: ${pos.left}`)
+      let text = `t=${pos.touches}:`
+      eventMap.forEach((v, k) => text += ` [${k}](${v.endX}, ${v.endY})`)
+      const textnode = document.createElement("p")
+      textnode.appendChild(document.createTextNode(text))
+      let consoleElement = document.getElementById("console")
+      //consoleElement.appendChild(textnode)
+      //consoleElement.appendChild()
+      //consoleElement.insertBefore(document.createElement("br"), consoleElement.children[0])
+      consoleElement.insertBefore(textnode, consoleElement.children[0])
+
+      var rect = ia1.getBoundingClientRect();
+      var r = Math.abs(rect.top-rect.bottom)/2
+      var cx = rect.left + Math.abs(rect.left-rect.right)/2
+      var cy = rect.top + r
+      //onsole.log(`rect: t=${rect.top}, l=${rect.left}, b=${rect.bottom}, r=${rect.right}`)
+      //console.log(`cx: ${cx}, cy: ${cy}, r: ${r}`)
+
+      var dist = (pos.endX - cx) * (pos.endX - cx) + (pos.endY - cy) * (pos.endY - cy)
+      console.warn(`end is ${dist < r*r ? 'within' : 'outside'} circle`)
+    })
+/*
+    var flattenTouches = function (ev) {
+      return ev.changedTouches.map(function(t) { return { ev: ev, touch: t }; });
+    }
+
+
+    var starts = window.rxjs.fromEvent(document, 'touchstart')
+        .selectMany(function (ev) { return window.rxjs.fromArray(flattenTouches(ev)); });
+    var moves = document.bindAsObservable("touchmove")
+        .selectMany(function (ev) { return window.rxjs.fromArray(flattenTouches(ev)); })
+        .publish().refCount(); // to prevent multiple subscriptions
+    var ends = document.bindAsObservable("touchend")
+        .selectMany(function (ev) { return window.rxjs.fromArray(flattenTouches(ev)); })
+        .publish().refCount(); // to prevent multiple subscriptions
+    
+    var moveOrEnds = window.rxjs.mergeObservable(moves, ends);
+    var timer = window.rxjs.timer(1500);
+    
+    var longpresses = starts
+        .selectMany(function (start) {
+            var thisPointerMovesOrEnds = movesOrEnds.where(function(t) {
+                return t.touch.identifier === start.touch.identifier;
+            });
+            return timer
+                .takeUntil(thisPointerMovesOrEnds)
+                .select(start);
+        });
+    
+    longpresses.subscribe(function (t) {
+      //console.log("longpress", t.touch.pageX, t.touch.pageY, t.touch.identifier);
+      const textnode = document.createElement("p")
+      textnode.appendChild(document.createTextNode(`t=${t.touches}: ${t.touch.pageX}, ${t.touch.pageY}`))
+      let consoleElement = document.getElementById("console")
+      consoleElement.appendChild(textnode)
+      consoleElement.appendChild()
+      consoleElement.insertBefore(document.createElement("br"), consoleElement.children[0])
+      consoleElement.insertBefore(textnode, consoleElement.children[0])
+    });*/
+  }
+})
